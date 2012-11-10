036e69069de419b7d97036a149a23cdbbec6428a
Apprentice <apprentice@dbc04.local>
2012-11-09 22:09:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasecampClone::Application.config.secret_token = '5e4d8d957c2597a420ccd6a27bf054f5f4bf9fe0f4724a70bb97c20902c29254c3e4d71656e3251c1bd704ca836e8f0cea2f4d5ee300cbf0d3ea5b0e2217dc5e'
